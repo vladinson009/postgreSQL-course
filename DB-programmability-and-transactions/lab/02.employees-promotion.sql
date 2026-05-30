c0aac2b4d1fe5f7e7cfadfae36ea00e9995ddb8b
Vladimir Gulev <vladinson009@icloud.com>
2026-05-30 17:24:14 +0200

    database programmability lab

---
+++ DB-programmability-and-transactions/lab/02.employees-promotion.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE sp_increase_salaries (department_name VARCHAR(20)) LANGUAGE plpgsql AS $$
+
+BEGIN
+ UPDATE employees
+    SET salary = salary * 1.05
+    WHERE department_id = (SELECT department_id FROM departments WHERE name = department_name);
+END;
+$$;
